2610d6aa8dadf1022724a4fa4331c8b064d1cb4f
pollockcl <pollock.clee@gmail.com>
2018-02-02 15:01:06 -0700

    Added tests for the node.

--- test/node_test.rb
+++ test/node_test.rb
@@ -1,10 +1,27 @@
-require_relative 'test_helper.rb'
+require_relative 'test_helper'
+require './lib/node'
 
 class NodeTest < Minitest::Test
 
   def test_node_exists
-    node = Node.new
+    node = Node.new('a')
+
     assert_instance_of Node, node
   end
 
+  def test_node_word_method
+    node = Node.new('s')
+
+    refute node.word?
+
+    node = Node.new('s', true)
+
+    assert node.word?
+  end
+
+  def test_node_inspect_displays_letter
+    node = Node.new('x')
+
+    assert_equal 'x', node.inspect
+  end
 end
