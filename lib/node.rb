51869c3f5f68d4a30b7c38c559a4db9a355ea235
pollockcl <pollock.clee@gmail.com>
2018-02-04 19:30:48 -0700

    added an attribute to keep track of the suffix weight for the select method.

--- lib/node.rb
+++ lib/node.rb
@@ -1,11 +1,12 @@
 # This is a node.
 class Node
   attr_reader :letter
-  attr_accessor :children, :word
+  attr_accessor :children, :word, :weight_holder
   def initialize(letter, word = false)
     @letter = letter
     @children = {}
     @word = word
+    @weight_holder = {}
   end
 
   def inspect
